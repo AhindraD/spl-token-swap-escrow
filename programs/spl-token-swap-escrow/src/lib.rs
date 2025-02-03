22a6bea890a85d68fe956d25ebb99af4f613e7e0
Ahindra D <official.ahindra@gmail.com>
2025-02-04 04:18:38 +0530

    workin-endpoint-fn

--- programs/spl-token-swap-escrow/src/lib.rs
+++ programs/spl-token-swap-escrow/src/lib.rs
@@ -1,9 +1,8 @@
+use anchor_lang::prelude::*;
 pub mod constants;
 pub mod error;
 pub mod instructions;
 pub mod state;
-
-use anchor_lang::prelude::*;
 
 pub use constants::*;
 pub use instructions::*;
@@ -15,8 +14,13 @@ declare_id!("BPRjA2PwsP31iYAExYYHuqDWU6yy4LtH1vPz4bzRRMgJ");
 pub mod spl_token_swap_escrow {
     use super::*;
 
-    pub fn make_offer(ctx: &Context<MakeOffer>) -> Result<()> {
-        instructions::make_offer::send_offered_tokens_to_vault()?;
-        instructions::make_offer::save_offer()?
+    pub fn make_offer(
+        ctx: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
     }
 }
